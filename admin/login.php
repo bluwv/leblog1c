4a7154838f986da303ba9bbb2b3de7f6d1b74ef6
bluwv <adrienpierre@aol.fr>
2026-05-11 19:50:05 +0200

    Integration design login page

--- admin/login.php
+++ admin/login.php
@@ -4,28 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../public/dist/style.css">
 </head>
-<body>
+
+<body data-page="login">
 
     <main>
-        <h1><a href="../public/index.php">Le Blog</a></h1>
-        <p>Don’t have an account yet? <a href="">Sign up</a></p>
+        <div class="container">
+            <h1><a href="../public/index.php">Le Blog</a></h1>
+            <p>Don’t have an account yet? <a href="">Sign up</a></p>
 
-        <form method="POST" action="list.php" novalidate autocomplete="off">
+            <form method="POST" action="list.php" novalidate autocomplete="off">
 
-            <label for="email">email address</label>
-            <input id="email" type="email" name="email" placeholder="name@gmail.com" autocomplete="username" required>
+                <div class="form-row">
+                    <label for="email">email address</label>
+                    <input id="email" type="email" name="email" placeholder="name@gmail.com" autocomplete="username" required>
+                </div>
 
-            <label for="password">password</label>
-            <input id="password" type="password" name="password" placeholder="******" autocomplete="current-password" required>
+                <div class="form-row">
+                    <label for="password">password</label>
+                    <input id="password" type="password" name="password" placeholder="******" autocomplete="current-password" required>
+                </div>
 
-            <input id="rememberme" type="checkbox">
-            <label for="rememberme">Se souvenir de moi</label>
+                <div class="form-row form-row--checkbox">
+                    <input id="rememberme" type="checkbox">
+                    <label for="rememberme">Se souvenir de moi</label>
+                </div>
 
-            <button type="submit">Login</button>
+                <button class="button" type="submit">Login</button>
 
-            <a href="">I lost my password</a>
-        </form>
+                <a href="">I lost my password</a>
+            </form>
+        </div>
 
         <a href="../public/index.php">Revenir au blog</a>
     </main>
