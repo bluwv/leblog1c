e69e4c30d9817954c2fe7a26c09977de72e7ee41
bluwv <adrienpierre@aol.fr>
2026-05-11 21:08:36 +0200

    Integration design homepage

--- public/inc/card-article.php
+++ public/inc/card-article.php
@@ -1,12 +1,12 @@
-<article>
-    <picture>
-        <img src="" alt="">
+<article class="card">
+    <picture class="card-thumbnail">
+        <img src="https://picsum.photos/seed/picsum/400/300" width="400" height="300" alt="">
     </picture>
 
-    <div>
-        <p>Prénom Nom</p>
-        <p>01/01/1970</p>
-        <h3><a href="single.php">test</a></h3>
+    <div class="card-content">
+        <span>Prénom Nom</span>
+        <time datetime="1970-01-01">01/01/1970</time>
+        <h3 class="title"><a href="single.php">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut accusamus odio neque delectus ex, eaque consectetur ratione, iusto itaque voluptate nam. At quo aperiam totam illum, accusamus quia beatae!</p>
     </div>
 </article>
--- public/index.php
+++ public/index.php
@@ -4,31 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="dist/style.css">
+    <link rel="stylesheet" href="dist/css/style.css">
 </head>
-<body>
 
-    <section>
-        <h1>Le Blog</h1>
+<body data-page="home">
+
+    <section class="hero">
+        <h1 class="title">Le Blog</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut accusamus odio neque delectus ex, eaque consectetur ratione, iusto itaque voluptate nam. At quo aperiam totam illum, accusamus quia beatae!</p>
     </section>
 
-    <section>
-        <h2>Latest blog posts</h2>
-        <?php include 'inc/card-article.php'; ?>
+    <section class="posts--lastest">
+        <h2 class="title">Latest blog posts</h2>
 
-         <?php
-            for ( $i = 0; $i < 3; $i++ ) {
-                include 'inc/card-article.php';
-            }
-        ?>
+        <div>
+            <?php include 'inc/card-article.php'; ?>
+
+            <?php
+                for ( $i = 0; $i < 3; $i++ ) {
+                    include 'inc/card-article.php';
+                }
+            ?>
+        <div>
     </section>
 
     <section class="posts--all">
-        <h2>All blog posts</h2>
+        <h2 class="title">All blog posts</h2>
 
-        <ul>
-            <li>
+        <ul class="filters">
+            <li class="active">
                 <a href="">all categories</a>
             </li>
             <li>
